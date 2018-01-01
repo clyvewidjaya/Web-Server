e40510d457f437f2e17a0532c94849bb2ba067c5
Clyve Widjaya <Clyve@Clyves-MacBook-Pro.local>
2018-01-01 02:48:43 -0500

    second update, setup ssh and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -57,7 +58,9 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(port,() => {
+  console.log(`Port is up on ${port}`);
+});
 
 //get needs 2 param, url, and function to run, what to sent back to the request
 //localhost:3000/
